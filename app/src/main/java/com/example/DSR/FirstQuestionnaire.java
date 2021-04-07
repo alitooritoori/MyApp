7923a38c394c4f30be653844c232e101a9270698
Faraz <alitoori.icp@gmail.com>
2021-04-07 02:10:54 -0400

    initial commit

---
+++ app/src/main/java/com/example/DSR/FirstQuestionnaire.java
@@ -0,0 +1,14 @@
+package com.example.DSR;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstQuestionnaire extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_questionnaire);
+    }
+}
---
+++ app/src/main/java/com/example/DSR/SecondQuestionnaire.java
@@ -0,0 +1,14 @@
+package com.example.DSR;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondQuestionnaire extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_questionnaire);
+    }
+}
